fa876dc5ef126f4cec575e56e22d4f6de349596a
Chris <fdq09eca@gmail.com>
2019-11-25 18:13:11 +0800

    07-simple-project finished, 25 Nov 2019

--- Flask-Bootcamp-master/03-Template-Basics/07-Simple-Project.py
+++ Flask-Bootcamp-master/03-Template-Basics/07-Simple-Project.py
@@ -1,23 +1,39 @@
 # Set up your imports and your flask app.
-
+from flask import Flask, request, render_template
+import re
+app = Flask(__name__)
 @app.route('/')
 def index():
     # This home page should have the form.
-    pass
+    return render_template('07-myform.html')
 
 
 # This page will be the page after the form
 @app.route('/report')
 def report():
+    username = request.args.get('user_name')
+    pattern = re.compile("(?=.*[a-z])(?=.*[A-Z]).*\d$")
+    match = re.match(pattern, username)
+
+    reasons = []
+    if len(username) == 0:
+        reasons.append('No username is typed')
+    else:
+        if not username[-1].isdigit():
+            reasons.append('Does not end with a number')
+        if not re.match('(?=.*[a-z]).*', username):
+            reasons.append('No lowercase')
+        if not re.match('(?=.*[A-Z]).*', username):
+            reasons.append('No uppercase')
+
+    return render_template('07-myreport_pos.html', username = username, match = match, reasons = reasons)
+    # else:
+    #     return render_template('07-myreport_neg.html', username = username)
+
     # Check the user name for the 3 requirements.
 
     # HINTS:
     # https://stackoverflow.com/questions/22997072/how-to-check-if-lowercase-letters-exist/22997094
     # https://stackoverflow.com/questions/26515422/how-to-check-if-last-character-is-integer-in-raw-input
-
-    # Return the information to the report page html.
-    pass
-
 if __name__ == '__main__':
-    # Fill this in!
-    pass
+    app.run(debug=True)
